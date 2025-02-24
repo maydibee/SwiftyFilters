f453904b567e21b6c7f92541426e3de777e7d790
Michael Skuratau <skuratau.m@gmail.com>
2025-02-24 10:25:00 +0800

    SF-3: workspace has been configured

---
+++ Examples/SwiftyFilters-Example-iOS/SwiftyFilters-Example-iOS/SwiftyFilters_Example_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyFilters_Example_iOSApp.swift
+//  SwiftyFilters-Example-iOS
+//
+//  Created by Софтрэйн on 23/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyFilters_Example_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
